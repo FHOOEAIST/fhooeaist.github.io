e940f3d3e2c595253f83c9243db174193aa474be
Andreas Pointner <andreas.pointner@fh-hagenberg.at>
2026-09-24 07:52:16 +0200

    trying to fix the github action

--- _plugins/img_baseurl.rb
+++ _plugins/img_baseurl.rb
@@ -1,8 +1,11 @@
 Jekyll::Hooks.register [:pages, :posts, :documents], :post_render do |doc|
   next unless doc.output_ext == ".html"
-  next if Jekyll.configuration({})["baseurl"].to_s.empty?
 
-  baseurl = doc.site.baseurl
+  # Use the effective baseurl (CLI --baseurl overrides _config.yml); a site
+  # served from the domain root ("" or "/") needs no rewriting.
+  baseurl = doc.site.baseurl.to_s.chomp("/")
+  next if baseurl.empty?
+
   # Prepend baseurl to any src="/..." that doesn't already start with baseurl
   doc.output.gsub!(/ src="(\/(?!#{Regexp.escape(baseurl[1..])}))/,
                    " src=\"#{baseurl}\\1")
